12e9c39da429df1a020747eec3e495b9c939700f
Bad0 <htbahadir@gmail.com>
2015-12-15 20:18:18 +0200

    Basic Toolbar start

--- app/src/main/java/com/example/bahadir/myapplicationn/AnaAkim.java
+++ app/src/main/java/com/example/bahadir/myapplicationn/AnaAkim.java
@@ -7,11 +7,16 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
@@ -21,6 +26,7 @@ public class AnaAkim extends AppCompatActivity {
     String isim;
     String resimurl;
     boolean notificationbas;
+    public Toolbar toolbar;
     public MaterialSearchView searchView;
     protected void onCreate(Bundle bambam) {
         super.onCreate(bambam);
@@ -28,7 +34,24 @@ public class AnaAkim extends AppCompatActivity {
         sharedkullaniciciktikaydet(false);
         notificationbas = true;
         notificationSharedPrefKaydet();
-
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        ImageButton imagesimge,imagearama;
+        ImageView logo;
+        final EditText etv1;
+        etv1 = (EditText) findViewById(R.id.aramaalani);
+        imagesimge = (ImageButton) findViewById(R.id.imageButton14);
+        imagearama = (ImageButton) findViewById(R.id.aramabutonu);
+        logo = (ImageView) findViewById(R.id.imageView13);
+        imagearama.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("tago", "onclicktıklandı");
+                editTextAyarla(etv1);
+               // final float scale = getResources().getDisplayMetrics().density;
+                // etv1.setWidth((int) (200 * scale + 0.5f));
+            }
+        });
         /*Intent intent = getIntent();
         bitmap = intent.getParcelableExtra("resim");
         isim =  intent.getStringExtra("isim");
@@ -46,6 +69,22 @@ public class AnaAkim extends AppCompatActivity {
        // yerolustur();
         //mesafeyiBul(x, y);
     }
+
+    private void editTextAyarla(EditText etv) {
+        Toolbar.LayoutParams lparams = new Toolbar.LayoutParams(50,30);
+        etv.setLayoutParams(lparams);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_anakim,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
     private void sharedkullaniciciktikaydet(boolean b) {
             SharedPreferences sP =getSharedPreferences("kullaniciverileri", Context.MODE_PRIVATE);
             SharedPreferences.Editor prefEditor = sP.edit();
